b2f6e910f212d3dbb02d610e209b217b2ca4cfbe
Teitan67 <oskrr67@gmail.com>
2022-08-12 17:46:00 -0600

    Agregado edpoint info y resta

--- src/calculadora.ts
+++ src/calculadora.ts
@@ -6,6 +6,14 @@ class Calculadora{
         const result    = data.num1 +data.num2
         return res.json({result:result})
     }
+    public async resta(req: Request, res: Response): Promise<Response>{
+        const data      = req.body;
+        const result    = data.num1 -data.num2
+        return res.json({result:result})
+    }
+    public async info(req: Request, res: Response): Promise<Response>{
+        return res.json({carnet:"201709144",nombre:"Oscar Roberto Velásquez León"})
+    }
 }
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { calculadora } from "./calculadora";
 const router = Router();
 
 router.route('/sumar').post(calculadora.sumar);
+router.route('/resta').post(calculadora.resta);
+router.route('/info').get(calculadora.info);
 
 
 export default router;
